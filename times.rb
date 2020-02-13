6186336e1a18a1c798fef0e5ce59c2c51cda523b
Daisy Olmo-Rivera <dazelee@yahoo.com>
2020-02-13 04:41:04 +0000

    Automatically backed up by Learn

--- times.rb
+++ times.rb
@@ -1,7 +1,14 @@
 def using_times
-	#your code here
-end
+  counter=0
+  loop
+ do
+   counter=counter+1
+  puts "#{counter}Windgardium Leviosa"
 
+	if counter>=7
+	  break#your code here
+end
+end
 
 
 
